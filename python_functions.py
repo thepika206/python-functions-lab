54f62485ff880533f7da901560659e4aed54f9fd
pcheung <pika206@gmail.com>
2022-11-01 14:35:39 -0500

    solve chall #2

--- python_functions.py
+++ python_functions.py
@@ -1,4 +1,4 @@
-# challenge 1 white a function named sum_to that accepts a single integer, n and returns the sum of the integers from 1 to n.
+# challenge 1 write a function named sum_to that accepts a single integer, n and returns the sum of the integers from 1 to n.
 
 def sum_to(n):
   i=1 
@@ -9,8 +9,20 @@ def sum_to(n):
   return solution
 
 # tests
+print('chall #1 tests, expect 21, 55')
 print(sum_to(6))
 print(sum_to(10))
 
 
-# challenge 2
+# challenge 2 Write a function named largest that takes a list of numbers as an argument and returns the largest number in that list.
+
+def largest(nums):
+  nums.sort()
+  return nums[-1]
+
+
+# tests
+print('chall #2, expect 4, 231')
+print(largest([1, 2, 3, 4, 0]))
+print(largest([10, 4, 2, 231, 91, 54]))
+
